b84752efaea50b6e957435c32888b5ea8af7cc49
Alexander J. Vincent <ajvincent@gmail.com>
2025-05-30 22:34:10 -0700

    Logging needs to be consistent between NodeJS and website.

--- docs/scripts/search/LoggingConfiguration.js
+++ docs/scripts/search/LoggingConfiguration.js
@@ -1,16 +1,43 @@
-import { LoggingConfiguration, } from "../../lib/packages/runSearchesInGuestEngine.js";
-export class SearchLogsConfiguration extends LoggingConfiguration {
+export class SearchLogsConfiguration {
     logsMap = new Map;
     #currentLogs;
+    noFunctionEnvironment = false;
+    internalErrorTrap() {
+        // eslint-disable-next-line no-debugger
+        debugger;
+    }
     beginSearch(sourceSpecifier, resultsKey) {
         this.#currentLogs = [];
         this.logsMap.set(resultsKey, this.#currentLogs);
+        this.log("enter " + sourceSpecifier + ": " + resultsKey, 0);
     }
     endSearch(sourceSpecifier, resultsKey) {
+        this.log("leave " + sourceSpecifier + ": " + resultsKey, 0);
         this.#currentLogs = undefined;
     }
+    enterNodeIdTrap(nodeId) {
+        this.log("enter search nodeId: " + nodeId, 1);
+    }
+    leaveNodeIdTrap(nodeId) {
+        this.log("leave search nodeId: " + nodeId, 1);
+    }
+    defineNodeTrap(parentId, weakKey, details) {
+        this.log(`defineNode: parentId=${parentId} weakKeyId=${weakKey} ${details}`);
+    }
+    defineEdgeTrap(parentId, edgeId, childId, secondParentId, isStrongReference) {
+        const secondIdPart = secondParentId ? " + " + secondParentId : "";
+        this.log(`defineEdgeTrap: ${parentId}${secondIdPart} via ${edgeId} to ${childId}, isStrongReference: ${isStrongReference}`);
+    }
+    defineWeakKeyTrap(weakKey) {
+        this.log("weak key defined: " + weakKey);
+    }
+    ;
+    markStrongNodeTrap(nodeId) {
+        this.log("mark strong node: " + nodeId);
+    }
+    ;
     log(message, indentLevel = 2) {
-        message = "  ".repeat(indentLevel - 1) + message;
+        message = "  ".repeat(indentLevel) + message;
         this.#currentLogs.push(message);
     }
 }
--- source/scripts/search/LoggingConfiguration.ts
+++ source/scripts/search/LoggingConfiguration.ts
@@ -1,22 +1,65 @@
-import {
-  LoggingConfiguration,
+import type {
+  SearchConfiguration,
 } from "../../lib/packages/runSearchesInGuestEngine.js";
 
-export class SearchLogsConfiguration extends LoggingConfiguration {
+export class SearchLogsConfiguration implements Required<SearchConfiguration> {
   public logsMap = new Map<string, string[]>;
   #currentLogs: string[] | undefined;
+
+  readonly noFunctionEnvironment = false;
+
+  internalErrorTrap(): void {
+    // eslint-disable-next-line no-debugger
+    debugger;
+  }
 
   beginSearch(sourceSpecifier: string, resultsKey: string): void {
     this.#currentLogs = [];
     this.logsMap.set(resultsKey, this.#currentLogs);
+    this.log("enter " + sourceSpecifier + ": " + resultsKey, 0);
   }
 
   endSearch(sourceSpecifier: string, resultsKey: string): void {
+    this.log("leave " + sourceSpecifier + ": " + resultsKey, 0);
     this.#currentLogs = undefined;
   }
 
+  enterNodeIdTrap(nodeId: string): void {
+    this.log("enter search nodeId: " + nodeId, 1);
+  }
+
+  leaveNodeIdTrap(nodeId: string): void {
+    this.log("leave search nodeId: " + nodeId, 1);
+  }
+
+  defineNodeTrap(parentId: string, weakKey: string, details: string): void {
+    this.log(`defineNode: parentId=${parentId} weakKeyId=${weakKey} ${details}`);
+  }
+
+  defineEdgeTrap(
+    parentId: string,
+    edgeId: string,
+    childId: string,
+    secondParentId: string | undefined,
+    isStrongReference: boolean
+  ): void
+  {
+    const secondIdPart = secondParentId ? " + " + secondParentId : "";
+    this.log(
+      `defineEdgeTrap: ${parentId}${secondIdPart} via ${edgeId} to ${childId}, isStrongReference: ${isStrongReference}`
+    );
+  }
+
+  defineWeakKeyTrap(weakKey: string): void {
+    this.log("weak key defined: " + weakKey);
+  };
+
+  markStrongNodeTrap(nodeId: string): void {
+    this.log("mark strong node: " + nodeId);
+  };
+
   log(message: string, indentLevel = 2): void {
-    message = "  ".repeat(indentLevel - 1) + message;
+    message = "  ".repeat(indentLevel) + message;
     this.#currentLogs!.push(message);
   }
 }
