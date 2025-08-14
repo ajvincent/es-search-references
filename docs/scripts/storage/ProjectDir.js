0177d2a9e3ad9989bdc04b896fc5b982e1366ba8
Alexander J. Vincent <ajvincent@gmail.com>
2025-08-14 08:03:44 -0700

    remove unused storage directory

--- docs/scripts/storage/ProjectDir.js
+++
@@ -1,3 +0,0 @@
-const rootDir = await navigator.storage.getDirectory();
-const ProjectDir = await rootDir.getDirectoryHandle("es-search-references", { create: true });
-export { ProjectDir };
--- source/scripts/storage/types/JSONStorageIfc.d.ts
+++
@@ -1,4 +0,0 @@
-export interface JSONStorageIfc {
-  removeItem(key: string): void;
-  setItem(key: string, value: Jsonifiable): void;
-}
